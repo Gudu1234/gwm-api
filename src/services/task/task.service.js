898ad01c22e9231fca34657b7c7bf830866124e6
soumyaranjansahoo338 <soumyaranjansahoo338@gmail.com>
2021-07-04 22:10:12 +0530

    "Minor changes."

--- src/services/task/task.service.js
+++ src/services/task/task.service.js
@@ -8,6 +8,7 @@ export default function (app) {
     const options = {
         Model: createModel(app),
         paginate: app.get('paginate'),
+        whitelist: ['$populate'],
     };
 
     // Initialize our service with any options it requires
